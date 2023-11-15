92532491f7434fe52a601416c81bb0363c264708
ErenDeSenYeter <106481273+ErenA-Dorrammu@users.noreply.github.com>
2023-11-15 10:26:34 +0000

    Updated some informations

--- sleeper.sh
+++ sleeper.sh
@@ -4,4 +4,39 @@ while true;
     do echo "Hanging out...";
     sleep 2;
     done
+#ls -l README.md && touch newfile.txt
+#ls
+#ls -l README.md && touch newfile2.txt
+#ls
+#LICENSE  README.md  bin.txt  hello.sh  newdir  newfile.txt  newfile2.txt  sleeper.sh
+
+
+#cat lines.txt | grep 3
+#cat lines.txt | grep 3 > match.txt
+
+
+
+#echo "something" > append.txt
+#cat append.txt
+#echo "and another thing" >> append.txt
+#cat append.txt
+
+#history
+#history | less
+#history | grep tail
+
+#ctrl + l = clears the terminal
+
+
+
+
+
+
+
+
+
+
+
+
+
 
